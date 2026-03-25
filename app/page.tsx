5949f43810406aedc2b0d7237ed44059f8f8e71f
Daniel Merrill <hi@danielmerrill.com>
2026-03-25 09:02:10 -0700

    add link

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,26 @@ export default function Home() {
     <div id="console">
       <main className="min-h-screen flex flex-col items-center justify-center">
         <h1>PICOPI.CC</h1>
-        <p>
-          A tiny next.js app running on a Raspberry Pi in the laundry room.
-        </p>
+        <p>A tiny next.js app running on a Raspberry Pi in the laundry room.</p>
         <ViewCounter id="homepage_views" />
+        <div
+          style={{
+            position: "absolute",
+            bottom: 10,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            left: 0,
+          }}
+        >
+          <a
+            href="/gameoftext"
+            className="text-green-400/70"
+            style={{ fontSize: "0.85rem", opacity: 0.85 }}
+          >
+            /game
+          </a>
+        </div>
       </main>
     </div>
   );
