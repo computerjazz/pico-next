d7dd6adffba8f9bcd1e74dd7604a12d3f737e9ce
Daniel Merrill <hi@danielmerrill.com>
2026-04-30 15:09:21 -0700

    make generateToken async

--- app/actions/generateToken.ts
+++ app/actions/generateToken.ts
@@ -2,7 +2,7 @@
 import jwt from "jsonwebtoken";
 
 // Just generate any old token the browser wants, for now
-export function generateToken({ scope }: { scope: string }) {
+export async function generateToken({ scope }: { scope: string }) {
   const wsToken = jwt.sign({ scope }, process.env.JWT_SECRET!);
   return wsToken;
 }
