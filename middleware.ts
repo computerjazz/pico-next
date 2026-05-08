4e13917376a928235c2c9884d06419227a41e397
Daniel Merrill <hi@danielmerrill.com>
2026-05-07 19:06:22 -0700

    add auth middleware

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,28 @@ import { NextResponse } from "next/server";
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  console.log("req.url", req.url, req.auth);
-  if (!req.auth) {
-    return NextResponse.redirect(new URL("/login", req.url));
+  const isLoginRoute = req.nextUrl.pathname.startsWith("/login");
+  if (req.auth) {
+    if (isLoginRoute) {
+      if (req.nextUrl.searchParams.has("redirect")) {
+        // If user is authenticated and on /login?redirect=... , redirect to specified location
+        const redirectTarget = req.nextUrl.searchParams.get("redirect")!;
+        return NextResponse.redirect(new URL(redirectTarget, req.url));
+      } else {
+        // redirect home
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+  } else {
+    const loginUrl = new URL("/login", req.url);
+    if (!loginUrl.searchParams.has("redirect") && !isLoginRoute) {
+      // Preserve the full path and query string
+      loginUrl.searchParams.set(
+        "redirect",
+        req.nextUrl.pathname + req.nextUrl.search,
+      );
+      return NextResponse.redirect(loginUrl);
+    }
   }
 });
 
@@ -17,8 +36,8 @@ export const config = {
       Match all request paths except for:
       - static files (_next, favicon, etc.)
       - API routes (/api)
-      - login page (/login)
     */
     "/device/:id*",
+    "/login",
   ],
 };
