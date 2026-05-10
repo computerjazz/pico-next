90ce957edcd2b68ff9d5deb808f0171bd4b53391
Daniel Merrill <hi@danielmerrill.com>
2026-05-10 14:11:57 -0700

    update audio

--- app/device/[id]/RecordingItem.tsx
+++ app/device/[id]/RecordingItem.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { getRecording } from "@/app/actions/getRecording";
+import { useStableCallback } from "@/app/hooks/useStableCallback";
 import { Recording } from "@/db/schema";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 
 function PlayCircle() {
   return (
@@ -47,23 +48,63 @@ function PauseCircle() {
   );
 }
 
-export default function RecordingItem({ recording }: { recording: Recording }) {
+function useAudio({ recordingId }: { recordingId: string }) {
   const [audioUrl, setAudioUrl] = useState<Buffer<ArrayBuffer> | null>(null);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const { id, createdAt } = recording;
-
   const audioRef = useRef<HTMLAudioElement>(null);
-  async function fetchAudio() {
-    setLoading(true);
+  const fetchAudio = useStableCallback(async () => {
+    if (isLoading) return;
+    setIsLoading(true);
     try {
-      const fileBuffer = await getRecording({ recordingId: id });
+      const fileBuffer = await getRecording({ recordingId });
       setAudioUrl(fileBuffer);
     } catch (e) {
       alert("Could not fetch audio");
+    } finally {
+      setIsLoading(false);
     }
-    setLoading(false);
-  }
+  });
+
+  const audioSource = useMemo(() => {
+    return audioUrl
+      ? URL.createObjectURL(
+          audioUrl instanceof Blob
+            ? audioUrl
+            : new Blob([audioUrl], { type: "audio/mpeg" }),
+        )
+      : undefined;
+  }, [audioUrl]);
+
+  const onPlay = useStableCallback(() => setIsPlaying(true));
+  const onPause = useStableCallback(() => setIsPlaying(false));
+
+  return {
+    audioUrl,
+    isLoading,
+    isPlaying,
+    audioRef,
+    audioSource,
+    fetchAudio,
+    onPlay,
+    onPause,
+  };
+}
+
+export default function RecordingItem({ recording }: { recording: Recording }) {
+  const { createdAt } = recording;
+
+  const {
+    isPlaying,
+    audioUrl,
+    fetchAudio,
+    audioRef,
+    audioSource,
+    onPlay,
+    onPause,
+  } = useAudio({
+    recordingId: recording.id,
+  });
 
   return (
     <div className="flex gap-2 justify-center">
@@ -89,17 +130,9 @@ export default function RecordingItem({ recording }: { recording: Recording }) {
 
       <audio
         ref={audioRef}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        src={
-          audioUrl
-            ? URL.createObjectURL(
-                audioUrl instanceof Blob
-                  ? audioUrl
-                  : new Blob([audioUrl], { type: "audio/mpeg" }),
-              )
-            : undefined
-        }
+        onPlay={onPlay}
+        onPause={onPause}
+        src={audioSource}
       />
     </div>
   );
