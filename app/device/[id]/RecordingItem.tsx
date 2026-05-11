68c0a6064255c10e44c8a96b2bc5669f2b4e9d7d
Daniel Merrill <hi@danielmerrill.com>
2026-05-10 21:19:25 -0700

    fix top-level await build issue

--- app/device/[id]/RecordingItem.tsx
+++ app/device/[id]/RecordingItem.tsx
@@ -1,52 +1,11 @@
 "use client";
 
 import { getRecording } from "@/app/actions/getRecording";
+import PauseCircle from "@/app/components/icons/PauseCircle";
+import PlayCircle from "@/app/components/icons/PlayCircle";
 import { useStableCallback } from "@/app/hooks/useStableCallback";
 import { Recording } from "@/db/schema";
 import { useMemo, useRef, useState } from "react";
-
-function PlayCircle() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-      />
-    </svg>
-  );
-}
-
-function PauseCircle() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-      />
-    </svg>
-  );
-}
 
 function useAudio({ recordingId }: { recordingId: string }) {
   const [audioUrl, setAudioUrl] = useState<Buffer<ArrayBuffer> | null>(null);
--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { CHANNEL_TYPE } from "@/lib/constants";
+import { CHANNEL_TYPE } from "../lib/constants";
 import { relations } from "drizzle-orm";
 import {
   pgSchema,
