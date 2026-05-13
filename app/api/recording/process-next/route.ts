19fbee53e2223582508f62e843966e5c0db16424
Daniel Merrill <hi@danielmerrill.com>
2026-05-13 07:04:35 -0700

    simplify

--- app/api/recording/process-next/route.ts
+++ app/api/recording/process-next/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { recordings } from "@/db/schema";
 import { verifyAuth } from "@/lib/auth";
 import { eq } from "drizzle-orm";
-import { transcribeAudio, processAudio } from "@/lib/audio";
+import { transcribeAudio, processAudio, getAudioDuration } from "@/lib/audio";
 
 export async function POST(req: Request) {
   const maybeResp = await verifyAuth(req, {
@@ -10,51 +10,53 @@ export async function POST(req: Request) {
   });
   if (maybeResp) return maybeResp;
 
-  const recordingMissingTranscript = await db.query.recordings.findFirst({
-    where: (t, { isNull }) => isNull(t.transcript),
+  let _transcript;
+  let _error;
+  let _durationMillis;
+
+  const actions: string[] = [];
+
+  const recording = await db.query.recordings.findFirst({
+    where: (t, { isNull, or }) =>
+      or(
+        isNull(t.transcript),
+        isNull(t.filepathProcessed),
+        isNull(t.durationMillis),
+      ),
     orderBy: (t, { asc }) => asc(t.createdAt),
   });
-
-  const recordingMissingProcessedMp3 = await db.query.recordings.findFirst({
-    where: (t, { isNull }) => isNull(t.filepathProcessed),
-  });
-
-  const recording = recordingMissingTranscript || recordingMissingProcessedMp3;
 
   if (!recording) {
     return new Response(null, { status: 404 });
   }
 
-  const action = recordingMissingTranscript ? "transcribe" : "process";
-
-  console.log(
-    `[${action}] start recording=${recording.id} filepath=${recording.filepath}`,
-  );
-
-  if (recordingMissingTranscript) {
+  if (!recording.transcript) {
     const { transcript, error } = await transcribeAudio({
-      filepath: recordingMissingTranscript.filepath,
+      filepath: recording.filepath,
     });
 
+    _transcript = transcript;
+    _error = error;
+
     if (transcript) {
-      console.log("transcription success!!", transcript);
       await db
         .update(recordings)
         .set({
           transcript,
         })
-        .where(eq(recordings.id, recordingMissingTranscript.id));
+        .where(eq(recordings.id, recording.id));
     }
-    return Response.json({ transcript, error });
-  } else if (recordingMissingProcessedMp3) {
-    const originalFilepath = recordingMissingProcessedMp3.filepath;
+    actions.push("transcript");
+  }
+
+  if (!recording.filepathProcessed) {
+    const originalFilepath = recording.filepath;
     const parts = originalFilepath.split(".");
     parts.pop();
     const withoutExt = parts.join(".");
     const outputPath = `${withoutExt}-processed.mp3`;
-    console.log(`processing ${outputPath}`);
     const { filepath: filepathProcessed } = await processAudio({
-      filepath: recordingMissingProcessedMp3.filepath,
+      filepath: originalFilepath,
       outputPath,
     });
     await db
@@ -62,7 +64,36 @@ export async function POST(req: Request) {
       .set({
         filepathProcessed,
       })
-      .where(eq(recordings.id, recordingMissingProcessedMp3.id));
-    return Response.json({ success: true });
+      .where(eq(recordings.id, recording.id));
+    actions.push("process-audio");
   }
+
+  if (!recording.durationMillis) {
+    const { durationMillis } = await getAudioDuration({
+      filepath: recording.filepath,
+    });
+    await db
+      .update(recordings)
+      .set({
+        durationMillis: String(durationMillis),
+      })
+      .where(eq(recordings.id, recording.id));
+    _durationMillis = durationMillis;
+    actions.push("duration");
+  }
+
+  console.log(
+    "recording/process-next actions:",
+    actions,
+    _transcript,
+    _durationMillis,
+    _error,
+  );
+
+  return Response.json({
+    transcript: _transcript,
+    error: _error,
+    durationMillis: _durationMillis,
+    actions,
+  });
 }
