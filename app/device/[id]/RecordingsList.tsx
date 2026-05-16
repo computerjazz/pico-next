97396d86985d412823e456fe90bb06292e3dc9b9
Daniel Merrill <hi@danielmerrill.com>
2026-05-16 08:40:28 -0700

    scroll to bottom

--- app/device/[id]/RecordingsList.tsx
+++ app/device/[id]/RecordingsList.tsx
@@ -3,14 +3,22 @@
 import { Recording } from "@/db/schema";
 import RecordingItem from "./RecordingItem";
 import { AudioProvider } from "@/app/components/AudioProvider";
+import { useLayoutEffect, useRef } from "react";
 
 export function RecordingsList({ recordings }: { recordings: Recording[] }) {
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    bottomRef.current?.scrollIntoView();
+  }, []);
+
   return (
     <AudioProvider>
       <div className="flex flex-col flex-1 gap-2">
         {recordings.map((r) => {
           return <RecordingItem key={r.id} recording={r} />;
         })}
+        <div ref={bottomRef} />
       </div>
     </AudioProvider>
   );
