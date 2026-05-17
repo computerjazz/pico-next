659b02dc1d5f3e688e354ecf78bbdbf59968c202
Daniel Merrill <hi@danielmerrill.com>
2026-05-16 21:36:24 -0700

    fade in on load

--- app/device/[id]/RecordingsList.tsx
+++ app/device/[id]/RecordingsList.tsx
@@ -3,18 +3,25 @@
 import { Recording } from "@/db/schema";
 import RecordingItem from "./RecordingItem";
 import { AudioProvider } from "@/app/components/AudioProvider";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
+import { useStableCallback } from "@/app/hooks/useStableCallback";
 
 export function RecordingsList({ recordings }: { recordings: Recording[] }) {
   const bottomRef = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const onLoad = useStableCallback(() => setIsVisible(true));
 
   useLayoutEffect(() => {
     bottomRef.current?.scrollIntoView();
-  }, []);
+    onLoad();
+  }, [onLoad]);
 
   return (
     <AudioProvider>
-      <div className="flex flex-col flex-1 gap-2">
+      <div
+        className={`flex flex-col flex-1 gap-2 transition-opacity duration-500 pb-16 ${isVisible ? "opacity-100" : "opacity-0"}`}
+      >
         {recordings.map((r) => {
           return <RecordingItem key={r.id} recording={r} />;
         })}
