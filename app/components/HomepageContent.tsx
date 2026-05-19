115f35ad76fa53b9ed9307482e2fcbd7febfbb71
Daniel Merrill <hi@danielmerrill.com>
2026-05-18 20:38:08 -0700

    src

--- app/components/HomepageContent.tsx
+++ app/components/HomepageContent.tsx
@@ -12,13 +12,15 @@ function HomepageContent() {
       <div
         className={`absolute flex top-0 left-0 right-0 bottom-0 pointer-none:*: -z-10 transition-opacity duration-500 ${isHovering ? "opacity-25" : "opacity-0"}`}
       >
-        <Image
-          src={hoveredCard?.src || ""}
-          alt={hoveredCard?.title || ""}
-          fill
-          className={`object-cover w-full h-full`}
-          priority
-        />
+        {!!hoveredCard?.src && (
+          <Image
+            src={hoveredCard.src}
+            alt={hoveredCard.title}
+            fill
+            className={`object-cover w-full h-full`}
+            priority
+          />
+        )}
       </div>
       <p>Surprisingly deep toys built from just one...</p>
       <div className="flex flex-row gap-4 p-4">
