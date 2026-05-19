83844451b06a699c083452f3d9c4f28f4e74287f
Daniel Merrill <hi@danielmerrill.com>
2026-05-19 14:42:36 -0700

    z-index

--- app/shortwave/[id]/page.tsx
+++ app/shortwave/[id]/page.tsx
@@ -136,7 +136,7 @@ export default async function DevicePage({
           </div>
         </div>
         <div
-          className="absolute flex flex-1 flex-row left-0 right-0 top-0 z-50 justify-center"
+          className="absolute flex flex-1 flex-row left-0 right-0 top-0 z-40 justify-center"
           style={{ transform: "translateY(-1px)" }}
         >
           <div className="flex bg-background flex-1" />
