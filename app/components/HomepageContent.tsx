c72de0fb266f4069adc9f74e26fb5b5b012ccced
Daniel Merrill <hi@danielmerrill.com>
2026-05-18 20:35:40 -0700

    hover

--- app/components/HomepageContent.tsx
+++ app/components/HomepageContent.tsx
@@ -5,22 +5,29 @@ import Image from "next/image";
 
 function HomepageContent() {
   const [hoveredCard, setHoveredCard] = useState<Card | null>(null);
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
     <main className="min-h-screen flex flex-col flex-1 items-center justify-center">
-      <div className="absolute flex top-0 left-0 right-0 bottom-0 pointer-none:*: -z-10">
+      <div
+        className={`absolute flex top-0 left-0 right-0 bottom-0 pointer-none:*: -z-10 transition-opacity duration-500 ${isHovering ? "opacity-25" : "opacity-0"}`}
+      >
         <Image
           src={hoveredCard?.src || ""}
           alt={hoveredCard?.title || ""}
           fill
-          className="object-cover w-full h-full transition-opacity duration-500"
-          style={{ opacity: hoveredCard?.src ? 0.25 : 0 }}
+          className={`object-cover w-full h-full`}
           priority
         />
       </div>
-      <p>Surprisingly deep toys from just one...</p>
+      <p>Surprisingly deep toys built from just one...</p>
       <div className="flex flex-row gap-4 p-4">
-        <HomepageCards onCardHover={setHoveredCard} />
+        <HomepageCards
+          onCardHover={(card) => {
+            if (card?.src) setHoveredCard(card);
+            setIsHovering(!!card);
+          }}
+        />
       </div>
     </main>
   );
