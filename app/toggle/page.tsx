96cd812f32c06b153db3b8b44cac3a4ec7877b9e
Daniel Merrill <hi@danielmerrill.com>
2026-05-26 13:41:58 -0700

    flip page

--- app/toggle/page.tsx
+++ app/toggle/page.tsx
@@ -7,11 +7,21 @@ function TogglePage() {
       <main className="flex flex-col gap-6">
         <PageHeader>
           <div className="flex flex-row gap-4 items-center">
-            <h1 className="text-3xl font-bold text-accent mb-2">toggle</h1>
+            <h1 className="text-3xl font-bold text-accent mb-2">flip</h1>
           </div>
         </PageHeader>
-        <HeroImage src="/img/hero-toggle.jpg" alt="toggle hero" />
-        <div className="p-4">Coming soon</div>
+        <HeroImage
+          src="/img/hero-toggle.jpg"
+          alt="toggle hero"
+          text="A gentle nudge from afar"
+        />
+        <div className="flex flex-col items-center">
+          <div className="gap-4 max-w-lg flex flex-col p-4">
+            <p>{`Put one switch on your countertop, and give the other to a friend. When they're both flipped the same way, the light stays green. When you give yours a flip, your friend's turns red.`}</p>
+            <p>{`How long will it take them to notice and flip them both to green again? Will they give theirs another flip and turn YOURS red?`}</p>
+            <p>{`Check the leaderboard to see who's spent the most time out of the red!`}</p>
+          </div>
+        </div>
       </main>
     </div>
   );
