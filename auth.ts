ff9d74fbda935adabf831611039b5d8cc1ab02b2
Daniel Merrill <hi@danielmerrill.com>
2026-07-16 09:25:42 -0700

    add role to session

--- auth.ts
+++ auth.ts
@@ -13,11 +13,15 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   }),
   callbacks: {
     jwt({ token, user }) {
-      if (user) token.id = user.id;
+      if (user) {
+        token.id = user.id || "";
+        token.role = user.role;
+      }
       return token;
     },
     session({ session, token }) {
       session.user.id = token.id as string;
+      session.user.role = token.role as string;
       return session;
     },
   },
