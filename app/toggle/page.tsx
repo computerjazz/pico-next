f3fbe4a85e506fbca48fe0eab83ac58bca12fb13
Daniel Merrill <hi@danielmerrill.com>
2026-07-25 14:11:57 -0700

    toggle copy

--- app/toggle/page.tsx
+++ app/toggle/page.tsx
@@ -9,7 +9,7 @@ function TogglePage() {
         <HeroImage
           src="/video/hero-toggle.jpg"
           alt="toggle hero"
-          text="Stay in sync from afar (or don't)"
+          text="Stay in sync from afar, or don't"
           videoPoster="/img/toggle-hero-poster.jpg"
           mediaClassName={`opacity-80`}
           videoSources={[
@@ -25,8 +25,8 @@ function TogglePage() {
         />
         <div className="flex flex-col items-center">
           <div className="gap-4 max-w-lg flex flex-col p-4">
-            <p>{`Keep one toggle and give the other to a friend. The light stays green as long as they're both flipped the same way. Give yours a flip to turn your friend's toggle red.`}</p>
-            <p>{`How long will it take them to notice? Will they return the toggles back to green, or will they flip theirs again and turn YOURS red?`}</p>
+            <p>{`Keep one Toggle and give the other to a friend. The light stays green as long as they're both flipped the same way. Flip yours to turn your friend's Toggle red.`}</p>
+            <p>{`How long will it take them to notice? Will they return the Toggles back to green, or will they flip theirs again and turn YOURS red?`}</p>
             <p>{`Check the leaderboard to see who's spent the most time out of the red!`}</p>
           </div>
         </div>
